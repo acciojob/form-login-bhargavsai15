f581fb54b2dcb54367e7c115d2fa030bd433405e
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-23 10:57:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const form = document.querySelectorAll('#form1>input');
+form[2].addEventListener('click', getFormvalue);
 function getFormvalue() {
-    //Write your code here
-
+  //Write your code here
+  const firstName = form[0];
+  const lastName = form[1];
+  alert(firstName.value + ' ' + lastName.value);
 }
